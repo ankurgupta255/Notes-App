0cdb436000e6ec7a07697dd793037c51cf003c29
Ankur Gupta <ankurgupta@Ankurs-Macbook-Pro-13.local>
2022-10-03 00:58:25 +0530

    Added Code Formatting using Prettier

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-const validator=require('validator');
-const chalk=require('chalk');
-const yargs=require('yargs');
-const notes=require('./notes.js')
+const validator = require("validator");
+const chalk = require("chalk");
+const yargs = require("yargs");
+const notes = require("./notes.js");
 
 // const command=process.argv[2];
 // console.log(process.argv);
@@ -13,92 +13,92 @@ const notes=require('./notes.js')
 // 	console.log('Removing Note');
 // }
 
-yargs.version('1.1.0')
+yargs.version("1.1.0");
 
 // Add command
 yargs.command({
-	command: 'add',
-	describe: 'Add a new note',
-	builder:{
-		title: {
-			describe: 'Note title',
-			demandOption: true,
-			type: 'string'
-		},
-		body: {
-			describe: 'Note content',
-			demandOption: true,
-			type: 'string'
-		}
-	},
-	handler: function (argv) {
-		// console.log('Title: ' + argv.title)
-		// console.log('Body: ' + argv.body)
-		notes.addNote(argv.title, argv.body)
-	}
-})
+  command: "add",
+  describe: "Add a new note",
+  builder: {
+    title: {
+      describe: "Note title",
+      demandOption: true,
+      type: "string",
+    },
+    body: {
+      describe: "Note content",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  handler: function (argv) {
+    // console.log('Title: ' + argv.title)
+    // console.log('Body: ' + argv.body)
+    notes.addNote(argv.title, argv.body);
+  },
+});
 
 // Remove Command
 yargs.command({
-	command: 'remove',
-	describe: 'Remove a note',
-	builder:{
-		title: {
-			describe: 'Note title',
-			demandOption: true,
-			type: 'string'
-		}
-	},
-	handler: function(argv){
-		notes.removeNote(argv.title)
-	}
-})
+  command: "remove",
+  describe: "Remove a note",
+  builder: {
+    title: {
+      describe: "Note title",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  handler: function (argv) {
+    notes.removeNote(argv.title);
+  },
+});
 
 // List command
 yargs.command({
-	command: 'list',
-	describe: 'List the notes',
-	handler(){
-		notes.listNotes()
-	}
-})
+  command: "list",
+  describe: "List the notes",
+  handler() {
+    notes.listNotes();
+  },
+});
 
 // Read command
 yargs.command({
-	command: 'read',
-	describe: 'Read a note',
-	builder:{
-		title: {
-			describe: 'Note title',
-			demandOption: true,
-			type: 'string'
-		}
-	},
-	handler: function(argv){
-		notes.readNotes(argv.title)
-	}
-})
+  command: "read",
+  describe: "Read a note",
+  builder: {
+    title: {
+      describe: "Note title",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  handler: function (argv) {
+    notes.readNotes(argv.title);
+  },
+});
 
 yargs.command({
-	command: 'edit',
-	describe: 'Edit a note',
-	builder:{
-		title: {
-			describe: 'Note title',
-			demandOption: true,
-			type: 'string'
-		},
-		body: {
-			describe: 'Note content',
-			demandOption: true,
-			type: 'string'
-		}
-	},
-	handler: function(argv){
-		notes.editNote(argv.title,argv.body)
-	}
-})
+  command: "edit",
+  describe: "Edit a note",
+  builder: {
+    title: {
+      describe: "Note title",
+      demandOption: true,
+      type: "string",
+    },
+    body: {
+      describe: "Note content",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  handler: function (argv) {
+    notes.editNote(argv.title, argv.body);
+  },
+});
 
-yargs.parse()
+yargs.parse();
 
 // console.log(yargs.argv);
